5b4a0dd5d0d9f1a0ec51f924a1ec52e2b5ee8b6e
ShiYanZhou <864834894@qq.com>
2022-06-24 10:34:45 +0800

    Create merge_excel.py

---
+++ merge_excel.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import pandas as pd
+
+
+def merge_excel(files):
+    res = {"Reference": [], "Gross invoice amount": []}
+    for file in files:
+        try:
+            data = pd.read_excel(file)
+            res["Reference"] += [str(s) for s in list(data["Reference"])]
+            res["Gross invoice amount"] += [str(s) for s in list(data["Gross invoice amount"])]
+        except:
+            continue
+    return pd.DataFrame(res)
+
+
+if __name__ == '__main__':
+    RES_FOLDER = sys.argv[1]
+    # TARGET_FOLDER = "C:\\Users\\I559057\\Desktop\\OCR_res"
+    FILE_LIST = os.listdir(RES_FOLDER)
+    FILE_LIST = [RES_FOLDER + "\\" + file for file in FILE_LIST]
+    merged_data = merge_excel(FILE_LIST)
+    writer = pd.ExcelWriter(RES_FOLDER+"\\result.xls")
+    merged_data.to_excel(writer)
+    writer.save()
+    writer.close()
+
